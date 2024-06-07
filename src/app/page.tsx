736f5dd89aaf0ae1481ee330013d0c6fe7409033
RuenChanPiseth <127710222+RuenChanPiseth@users.noreply.github.com>
2024-06-07 18:03:10 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit deleniti minima, sed molestias eveniet temporibus
+        voluptates ducimus facilis inventore harum expedita corporis sint perferendis! Delectus officia repudiandae
+        possimus ipsam rem.s
+      </p>
+    </main>
+  );
+}
