acab6389f2049a64709623be5eb8033be9ebd933
RuenChanPiseth <127710222+RuenChanPiseth@users.noreply.github.com>
2024-06-07 18:05:32 +0700

    start

---
+++ src/components/button/DisabledButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisabledButton = () => {
+  return <div></div>;
+};
+
+export default DisabledButton;
---
+++ src/components/home/Generate.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Generate = () => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Generate;
